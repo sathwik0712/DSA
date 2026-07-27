bb223e922deab4070ccf71791864cc48fd107018
sathwik <sathwik0712@gmail.com>
2026-07-27 21:20:53 +0530

    Time: 1 ms (37.11%), Space: 95.1 MB (80.05%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int n=n1+n2;
+        int i=0,j=0;
+        int ind2=n/2;
+        int ind1=ind2-1;
+        int cnt=0;
+        int ind1ele=-1,ind2ele=-1;
+        while(i<n1 && j<n2){
+            if(nums1[i]<nums2[j] ){
+                if(cnt==ind1) 
+                    ind1ele=nums1[i];
+                if(cnt==ind2)
+                    ind2ele=nums1[i];
+                cnt++;
+                i++;
+            }else{
+                if(cnt==ind1) 
+                    ind1ele=nums2[j];
+                if(cnt==ind2)
+                    ind2ele=nums2[j];
+                cnt++;
+                j++;
+            }
+        }
+        while(i<n1){
+            if(cnt==ind1)
+                ind1ele=nums1[i];
+            if(cnt==ind2)
+                ind2ele=nums1[i];
+            cnt++;
+            i++;
+        }
+        while(j<n2){
+            if(cnt==ind1)
+                ind1ele=nums2[j];
+            if(cnt==ind2)
+                ind2ele=nums2[j];
+            cnt++;
+            j++;
+        }
+        if(n%2==1){
+            return ind2ele;
+        }
+        return (double)((double)(ind1ele+ind2ele))/2.00;
+    }
+};
