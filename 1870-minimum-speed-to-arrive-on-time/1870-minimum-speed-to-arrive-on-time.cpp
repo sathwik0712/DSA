70df400d6f00aa858a5b24e8f8d8843af389a5b3
sathwik <sathwik0712@gmail.com>
2026-08-05 21:32:51 +0530

    Time: 89 ms (15.14%), Space: 105.6 MB (23.83%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     double fn(vector<int>&dist,int h){
         double total=0;
-        for(int i=0;i<dist.size()-1;i++){
+        for(int i=0;i<dist.size()-1;i++)
             total+=ceil((double)dist[i] / h);
-        }
         total+=(double)dist.back() / h;
         return total;
     }
@@ -13,11 +12,10 @@ public:
         while(low<=high){
             int mid=(low+high)/2;
             double totalH=fn(dist,mid);
-            if(totalH<=hour){
+            if(totalH<=hour)
                 high=mid-1;
-            }else{
+            else
                 low=mid+1;
-            }
         }
         if ( low>1e7)
             return -1;
